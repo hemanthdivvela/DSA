3cc0fb3b816538d6a43e6144d1372d7ec7cb923a
hemanth <divvelahemanth7@gmail.com>
2023-01-31 09:56:37 +0530

    singlelinkedlist.go

--- linkedlist/singlelinkedlist.go
+++ linkedlist/singlelinkedlist.go
@@ -83,30 +83,102 @@ func (l *linkedlist) addany(n int, pos int) {
 	newnode.next = p.next
 	p.next = newnode
 }
+func (l *linkedlist) delete_first() {
+	if l.head == nil {
+		fmt.Print("it is empty")
+		return
+	}
+	//l.head.data = l.head.data
+	l.head = l.head.next
+	if l.head == nil {
+		l.tail = nil
+		return
+	}
+
+}
+
+func (l *linkedlist) delete_end() {
+	if l.head == nil {
+		fmt.Print("it is empty")
+		return
+	}
+	p := l.head
+	i := 1
+	for i < l.len()-1 {
+		p = p.next
+		i++
+	}
+	l.tail = p
+	p = p.next
+	//l.head.data = l.head.data
+	l.tail.next = nil
+	return
+
+}
+
+func (l *linkedlist) delete_any(pos int) {
+	if l.head == nil {
+		fmt.Print("it is empty")
+		return
+	}
+	p := l.head
+	i := 1
+	for i < pos {
+		p = p.next
+		i++
+	}
+	p.data = p.next.data
+	p.next = p.next.next
+	return
+
+}
 
 func main() {
 	l := linkedlist{}
-	l.isEmpty()
-	fmt.Println()
 	l.push(10)
 	l.push(20)
 	l.push(30)
 	l.push(40)
 	l.display()
-	fmt.Print()
-	k := l.len()
-	fmt.Print()
+	a := l.len()
 	fmt.Println()
-	fmt.Print(k)
+	fmt.Print(a)
 	fmt.Println()
+	l.addfirst(8)
 	l.addfirst(5)
-	l.addfirst(6)
 	l.display()
+	b := l.len()
 	fmt.Println()
-	l.addlast(9)
-	l.display()
+	fmt.Print(b)
 	fmt.Println()
-	l.addany(0, 4)
+	l.addlast(55)
 	l.display()
+	c := l.len()
+	fmt.Println()
+	fmt.Print(c)
+	fmt.Println()
+	l.addany(25, 4)
+	l.display()
+	d := l.len()
+	fmt.Println()
+	fmt.Print(d)
+	fmt.Println()
+	l.delete_first()
+	l.display()
+	e := l.len()
+	fmt.Println()
+	fmt.Print(e)
+	fmt.Println()
+	l.delete_end()
+	l.display()
+	f := l.len()
+	fmt.Println()
+	fmt.Print(f)
+	fmt.Println()
+	l.delete_any(5)
+	l.display()
+	g := l.len()
+	fmt.Println()
+	fmt.Print(g)
 
 }
